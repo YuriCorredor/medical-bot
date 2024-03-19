b5dc9c0c1079be5d64145c946c04beb07ef5d6a5
YuriCorredorFocus <yuri.corredor@focusnfe.com.br>
2024-03-19 20:44:08 -0300

    feat: add new files and update requirements.txt

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+  name = 'Medical Chatbot',
+  version= '0.0.1',
+  author= 'Yuri Corredor',
+  packages= find_packages(),
+  install_requires = []
+)
